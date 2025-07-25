65a804dc7aa6f95a847a4b574c1d2efc472e4832
jcrews123 <168486450+jcrews123@users.noreply.github.com>
2025-07-24 20:14:40 -0400

    added drizzle

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import rigoImageUrl from "../assets/img/rigo-baby.jpg";
+import drizzle from "../assets/img/image/drizzle";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
 export const Home = () => {
@@ -7,10 +7,10 @@ export const Home = () => {
 
 	return (
 		<div className="text-center mt-5">
-			<h1>TEST</h1>
+			<h1>American Drizzle</h1>
 			<p>
-				<img src={rigoImageUrl} />
+				<img src={drizzle} />
 			</p>
 		</div>
 	);
-}; 
+};
